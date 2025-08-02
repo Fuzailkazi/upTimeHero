c2b23d9c84234ac76115c54cc00acd59bf6e69f7
Fuzail Kazi <mohdfuzailkazi14@gmail.com>
2025-08-02 15:59:25 +0530

    a few changes in website test

--- uth/apps/tests/website.test.ts
+++ uth/apps/tests/website.test.ts
@@ -1,13 +1,28 @@
-import { describe, it, expect } from "bun:test";
+import { describe, it, expect, beforeAll } from "bun:test";
 import axios from "axios";
+import { createUser } from "./testUtils";
 
-const DATABASE_URL = "http://localhost:3000";
+const BACKEND_URL = "http://localhost:3000";
+
+let jwt:string, token:string;
+
+beforeAll(async ()=>{
+    const data = await createUser()
+    jwt = data.jwt,
+    token = data.jwt
+})
 
 describe("Website gets created", () => {
     it("Website not created if url is not present", async () => {
         try {
-            await axios.post(`${DATABASE_URL}/website`, {});
-            expect(false, "website created with no url");
+            await axios.post(`${BACKEND_URL}/website`, {
+                
+            }, {
+                headers: {
+                    Authorization: token
+                }
+            });
+            expect(false, "Website created when it shouldnt");
         } catch (error) {
             console.log(error); // expected: server should throw error for missing URL
         }
@@ -15,10 +30,25 @@ describe("Website gets created", () => {
 
     it.todo("Website is created if url is not present", async () => {
 
-        const response = await axios.post(`${DATABASE_URL}/website`, {
-            url: "https://google.com",
-        });
+        const response = await axios.post(`${BACKEND_URL}/website`, {
+            url: "https://google.com"
+        }, {
+            headers: {
+                Authorization: token
+            }
+        })
         expect(response.data.id).not.toBeNull();
 
     });
+
+    it("Website is not created if the header is not present", async () => {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/website`, {
+                url: "https://google.com"
+            });
+            expect(false, "Website shouldnt be created if no auth header")
+        } catch(e) {
+
+        }
+    })
 });
